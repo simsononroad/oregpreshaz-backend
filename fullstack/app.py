6f98e99e66502db952d3924dd22858815bc87ab9
simsononroad <d-github@kopaszhegy.hu>
2024-12-22 13:54:27 +0100

    main pages

--- fullstack/app.py
+++ fullstack/app.py
@@ -13,5 +13,22 @@ def index():
 def info():
     return render_template('aloldalak/info.html')
 
+@app.route('/galeria')
+def galeria():
+    return render_template('aloldalak/galery.html')
+
+@app.route('/kikapcsolodasi_lehetosegek')
+def kikapcsolodasi_lehetosegek():
+    return render_template('aloldalak/kikapcs.html')
+
+@app.route('/elerhetosegek')
+def elerhetosegek():
+    return render_template('aloldalak/elerhetosegek.html')
+
+@app.route('/send_message')
+def sand_message():
+    return render_template('aloldalak/sand_message.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
