52806a3e706102faa0ff1a7ccbdf408cb88f004a
simsononroad <d-github@kopaszhegy.hu>
2024-12-22 14:57:52 +0100

    refactor: remove unused HTML file and update routes for new pages

--- fullstack/app.py
+++ fullstack/app.py
@@ -4,59 +4,71 @@ app = Flask(__name__)
 app.template_folder = 'templates'
 app.static_folder = 'static'
 
+class path():
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/informacio')
+    def info():
+        return render_template('aloldalak/info.html')
 
-@app.route('/informacio')
-def info():
-    return render_template('aloldalak/info.html')
+    @app.route('/galeria')
+    def galeria():
+        return render_template('aloldalak/galery.html')
 
-@app.route('/galeria')
-def galeria():
-    return render_template('aloldalak/galery.html')
+    @app.route('/kikapcsolodasi_lehetosegek')
+    def kikapcsolodasi_lehetosegek():
+        return render_template('aloldalak/kikapcs.html')
 
-@app.route('/kikapcsolodasi_lehetosegek')
-def kikapcsolodasi_lehetosegek():
-    return render_template('aloldalak/kikapcs.html')
+    @app.route('/elerhetosegek')
+    def elerhetosegek():
+        return render_template('aloldalak/elerhetosegek.html')
 
-@app.route('/elerhetosegek')
-def elerhetosegek():
-    return render_template('aloldalak/elerhetosegek.html')
+    @app.route('/send_message')
+    def sand_message():
+        return render_template('aloldalak/sand_message.html')
 
-@app.route('/send_message')
-def sand_message():
-    return render_template('aloldalak/sand_message.html')
+    @app.route('/kepek/furdo')
+    def furdo():
+        return render_template('aloldalak/imagepage/furdo.html')
 
-@app.route('/kepek/furdo')
-def furdo():
-    return render_template('aloldalak/imagepage/furdo.html')
+    @app.route('/kepek/kint')
+    def kint():
+        return render_template('aloldalak/imagepage/kint.html')
 
-@app.route('/kepek/kint')
-def kint():
-    return render_template('aloldalak/imagepage/kint.html')
+    @app.route('/kepek/konyha')
+    def konyha():
+        return render_template('aloldalak/imagepage/konyha.html')
 
-@app.route('/kepek/konyha')
-def konyha():
-    return render_template('aloldalak/imagepage/konyha.html')
+    @app.route('/kepek/nappali')
+    def nappali():
+        return render_template('aloldalak/imagepage/nappali.html')
 
-@app.route('/kepek/nappali')
-def nappali():
-    return render_template('aloldalak/imagepage/nappali.html')
+    @app.route('/kepek/szoba')
+    def szoba():
+        return render_template('aloldalak/imagepage/szoba.html')
 
-@app.route('/kepek/szoba')
-def szoba():
-    return render_template('aloldalak/imagepage/szoba.html')
+    @app.route('/kepek/telikert')
+    def telikert():
+        return render_template('aloldalak/imagepage/telikert.html')
 
-@app.route('/kepek/telikert')
-def telikert():
-    return render_template('aloldalak/imagepage/telikert.html')
+    @app.route('/kepek/udvar')
+    def udvar():
+        return render_template('aloldalak/imagepage/udvar.html')
 
-@app.route('/kepek/udvar')
-def udvar():
-    return render_template('aloldalak/imagepage/udvar.html')
+    @app.route('/segitseg')
+    def segitseg():
+        return render_template('aloldalak/help.html')
 
+    @app.route('/kikapcsolodasi_lehetosegek/motorozas')
+    def motorozas():
+        return render_template('aloldalak/moto.html')
+
+    @app.route('/sport')
+    def sport():
+        return render_template('aloldalak/sport.html')
+path()
 
 if __name__ == '__main__':
     app.run(debug=True)
